7ead551ca80f84201b7401bd2c1e64affd57ae14
root <root@srv751139.hstgr.cloud>
2026-04-03 19:59:04 +0000

    server

--- dashtar/dist/@/assets/PageTitle-DuF0k8KR.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-WS6oPXgt.js";const s=({children:t})=>e.jsx("h1",{className:"my-6 text-2xl font-bold text-gray-700 dark:text-gray-300 font-serif",children:t});export{s as P};
